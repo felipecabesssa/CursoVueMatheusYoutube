25a1447185e8d2430f616e96751d39bb24cb9ebc
felipecabesssa <felipesoares100@hotmail.com>
2023-02-23 10:29:29 -0300

    Data - Dados em componentes

--- projeto_vue/src/components/PrimeiroComponente.vue
+++ projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
     <h1>Olá Vue!</h1>
+    <h3>Meu nome é: {{ nome }} {{ sobrenome }}</h3>
+    <p><strong>Profissão:</strong> {{ profissao }}</p>
 </template>
 
 <script>
 export default {
-    name: 'PrimeiroComponente'
+    name: 'PrimeiroComponente',
+    data(){
+        return{
+            nome: "Felipe",
+            sobrenome: "Henrique",
+            profissao: "Desenvolvedor WEB"
+        }
+    }
 }
 </script>
