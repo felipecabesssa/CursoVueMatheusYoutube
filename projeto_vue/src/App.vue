ed8da1506f3900344e38b43a5e5d52b8a2080673
felipecabesssa <felipesoares100@hotmail.com>
2023-02-24 15:03:31 -0300

    LifeCycle Hooks - Ciclo de vida (gancho)

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,14 +1,24 @@
 <template>
+  <hr>
+  <h3>----- Aula 1 a 6 - Components -----</h3>
+  <hr>
   <primeiroComponente />
+  <hr>
+  <h3>----- Aula 7 - LifeCycle Hooks - Gancho de Ciclo de vida -----</h3>
+  <hr>
+  <p>Muda o nome a cada 2 segundos (this.nome dentro do setTimeOut)</p>
+  <lyfeCycle />
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue' 
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LyfeCycle from './components/LifeCycle.vue' 
 
 export default {
   name: 'App',
   components: {
-    PrimeiroComponente
+    PrimeiroComponente,
+    LyfeCycle
   }
 }
 </script>
---
+++ projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LyfeCycle',
+    data(){
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = 'Felipe'
+        }, 2000)
+    },
+    mounted(){
+        setTimeout(() => {
+            this.nome = 'Matheus'
+        }, 4000)
+    }
+}
+</script>
