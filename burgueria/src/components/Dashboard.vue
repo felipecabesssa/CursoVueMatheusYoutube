40d974aac833097b8e4c1bf9738929c5e4c8272b
felipecabesssa <felipesoares100@hotmail.com>
2023-03-27 21:07:12 -0300

    Deletar pedido - Btn Cancelar

--- burgueria/src/components/Dashboard.vue
+++ burgueria/src/components/Dashboard.vue
@@ -30,7 +30,7 @@
               {{ s.tipo }}
             </option>
           </select>
-          <button class="delete-btn">Cancelar</button>
+          <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
       </div>
 
@@ -58,6 +58,7 @@ export default {
       this.burgers = data;
 
       console.log(this.burgers);
+
       // Resgatar o status
       this.getStatus();
 
@@ -70,8 +71,19 @@ export default {
 
       this.status = data;
 
-      console.log(data);
+    },
+    async deleteBurger(id){
 
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE"
+      });
+
+      const res = await req.json();
+
+      //msg de status
+
+      this.getPedidos();
+      
     }
   },
   mounted() {
@@ -82,7 +94,7 @@ export default {
 
 <style scoped>
   .container {
-    width: 485px;
+    width: 600px;
     margin-left: -45px;
   }
 
